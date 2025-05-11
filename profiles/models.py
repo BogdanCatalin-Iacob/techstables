677ce4f678b08d772a1480217215bc6b2385bd46
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-05-11 20:41:18 +0100

    Define profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(
+        upload_to='images/', default='media/images/default_profile_xdfle7')
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
