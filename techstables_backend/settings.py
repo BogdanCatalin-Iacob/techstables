ed321bd787bfc8b89e167d00a12a977ede611e85
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-05-14 00:07:27 +0100

    Update secret key to get it from env file

--- techstables_backend/settings.py
+++ techstables_backend/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&pr==pbl&*0kw&8l2k4^8!9vay#t89(*jkfkxnveyz2w^!ujp6'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
