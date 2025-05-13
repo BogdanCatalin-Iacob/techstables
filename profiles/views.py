0b6b0ad00605c869b1549e1a38561520bf8d1489
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-05-13 23:51:34 +0100

    Create ProfileList view with generics

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
