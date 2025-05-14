7818f7efe0e8ddb5b759eb2daa69b99ca445b487
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-05-14 01:52:46 +0100

    Create tests for profile list view

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .views import ProfileList
 
-# Create your tests here.
+
+class ProfileListTests(APITestCase):
+    def setUp(self):
+        # Create test users
+        User.objects.create_user(username='user1', password='pass1')
+        User.objects.create_user(username='user2', password='pass2')
+
+    def test_can_list_all_profiles(self):
+        response = self.client.get('/profiles/')
+        view = ProfileList()
+        queryset = view.get_queryset()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(queryset.count(), 2)
