2ca20e1ce02a9f7b2ffca7ed60c0e9392105f99d
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-05-14 03:23:37 +0100

    Add Profile model as StackedInline to the User model in the admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    '''
+    Defines an inline admin interface for the Profile model, allowing
+    profiles to be edited directly within the User admin page.
+    '''
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    '''
+    Customizes the admin interface for the User model by specifying
+    fields to display and integrating the ProfileInline to allow
+    editing of related Profile instances directly within the User
+    admin page.
+    '''
+    model = User
+
+    # fields = ['username']
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
