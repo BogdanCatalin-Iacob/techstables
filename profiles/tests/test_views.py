ec7fa59346ae1289597800b786fe52c629a054a2
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-05-15 22:31:55 +0100

    Create profiles views tests

---
+++ profiles/tests/test_views.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase, APIRequestFactory
+from ..views import ProfileList, ProfileDetail
+from ..models import Profile
+
+
+class ProfileListTests(APITestCase):
+    def setUp(self):
+        # Create test users
+        User.objects.create_user(username='user1', password='pass1')
+        User.objects.create_user(username='user2', password='pass2')
+
+    def test_can_list_all_profiles(self):
+        response = self.client.get('/profiles/')
+        view = ProfileList()
+        queryset = view.get_queryset()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(queryset.count(), 2)
+
+
+class ProfileDetailTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='user1', password='pass1')
+
+    def test_logged_out_user_cant_edit_profile(self):
+        profile = Profile.objects.get(pk=1)
+        factory = APIRequestFactory()
+        request = factory.post(f'/profiles/{profile.id}', {'name': 'Nick'})
+        view = ProfileDetail.as_view()
+
+        response = view(request, pk=1)
+        self.assertEqual(
+            response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_can_retrieve_profile_using_valid_id(self):
+        profile = Profile.objects.get(pk=1)
+        factory = APIRequestFactory()
+        request = factory.get(f'/profiles/{profile.id}/')
+        view = ProfileDetail.as_view()
+        response = view(request, pk=profile.id)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_nonexistent_profile(self):
+        factory = APIRequestFactory()
+        request = factory.get('/profiles/9999/')
+        view = ProfileDetail.as_view()
+        response = view(request, pk=9999)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
