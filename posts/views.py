0fdd82a96e7fb065f501f4d1d53a83b2cc40bd93
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-05-20 20:44:08 +0100

    Define list/create posts view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    '''
+    API view for listing and creating Post instances.
+
+    This view allows authenticated users to create new posts and
+    provides a read-only list of all posts to unauthenticated users.
+    Utilizes the PostSerializer for serialization and enforces
+    permissions using IsAuthenticatedOrReadOnly.
+    '''
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all()
