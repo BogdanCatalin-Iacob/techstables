ae1350462f12f84c8ada32164d6a449c14a4b9c3
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-05-26 05:51:02 +0100

    Define CommentList view

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    '''
+    API view for listing and creating comments.
+
+    This view provides a list of all comments and allows authenticated users
+    to create new comments. It uses the CommentSerializer for serialization
+    and enforces permissions such that only authenticated users can create
+    comments, while others can only read.
+
+    Attributes:
+        serializer_class (CommentSerializer): The serializer used for the comments.
+        permission_classes (list): Permissions required to access the view.
+        queryset (QuerySet): The base queryset for retrieving comments.
+
+    Methods:
+        perform_create(serializer): Saves a new comment with the current user as the owner.
+    '''
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
