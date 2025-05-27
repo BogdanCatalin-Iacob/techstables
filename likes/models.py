6a13ee579a50c21a2e651e217db6e887801a7b1a
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-05-27 01:25:55 +0100

    Refactor DateTimeField auto_add_now=True

--- likes/models.py
+++ likes/models.py
@@ -23,7 +23,7 @@ class Like(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
         Post, related_name='likes', on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_at']
