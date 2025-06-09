5731d5c155e17e9f7ad513426400667087dc2870
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-06-09 21:12:13 +0100

    Add connection to postgresql db server

--- techstables_backend/settings.py
+++ techstables_backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 if os.path.exists('env.py'):
     import env
+import dj_database_url
 
 
 # Cloudinary storage
@@ -127,12 +128,17 @@ WSGI_APPLICATION = 'techstables_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
