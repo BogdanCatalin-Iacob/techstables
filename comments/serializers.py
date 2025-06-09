e1bba2a7e5b180b5fbc345b23212e3611e02c1ad
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-06-09 19:41:06 +0100

    Add natural time (as 3 hours ago) for comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -9,12 +10,18 @@ class CommentSerializer(serializers.ModelSerializer):
 
     Attributes:
         owner (ReadOnlyField): The username of the comment owner.
-        is_owner (SerializerMethodField): Indicates if the request user is the owner.
+        is_owner (SerializerMethodField):
+            Indicates if the request user is the owner.
         profile_id (ReadOnlyField): The ID of the owner's profile.
         profile_image (ReadOnlyField): The URL of the owner's profile image.
+        created_at (SerializerMethodField): Human-readable creation time.
+        updated_at (SerializerMethodField): Human-readable last update time.
 
     Methods:
-        get_is_owner(obj): Determines if the request user is the owner of the comment.
+        get_is_owner(obj): Determines if the request user
+            is the owner of the comment.
+        get_created_at(obj): Returns a human-readable creation time.
+        get_updated_at(obj): Returns a human-readable last update time.
 
     Meta:
         model (Comment): The model that is being serialized.
@@ -24,10 +31,49 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
+        '''
+        Determines if the current request user is
+            the owner of the given object.
+
+        Args:
+            obj: The object instance being serialized.
+
+        Returns:
+            bool: True if the request user is the owner of the object, False otherwise
+        '''
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        '''
+        Returns a human-readable representation of the object's creation time.
+
+        Args:
+            obj: The object instance being serialized.
+
+        Returns:
+            str: A natural language representation of the creation time,
+                such as "3 days ago".
+        '''
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        '''
+        Returns a human-readable representation of the object's last
+            update time.
+
+        Args:
+            obj: The object instance being serialized.
+
+        Returns:
+            str: A natural language representation of the last update time,
+                such as "2 hours ago".
+        '''
+        return naturaltime(obj.updated_at)
 
     class Meta:
         model = Comment
