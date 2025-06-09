a245550967c81e1e11f8a7cfcb8c714f807220ae
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-06-09 19:33:44 +0100

    Set default renderer as json in production

--- techstables_backend/settings.py
+++ techstables_backend/settings.py
@@ -72,12 +72,18 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )]
 }
+
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_AUTH = {
     'USE_JWT': True,
