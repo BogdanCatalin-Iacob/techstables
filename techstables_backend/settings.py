92e2a73e92bd3f373a25168e98fb0fc1a8b508b2
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-06-10 18:42:59 +0100

    Add Procfile, and cors headers settings. Update requirements file

--- techstables_backend/settings.py
+++ techstables_backend/settings.py
@@ -36,9 +36,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -91,10 +92,12 @@ REST_AUTH = {
     'JWT_AUTH_SECURE': True,
     'JWT_AUTH_COOKIE': 'techstables_auth',
     'JWT_AUTH_REFRESH_COOKIE': 'techstables_refresh',
+    'JWT_AUTH_SAMESITE': 'None',
     'USER_DETAILS_SERIALIZER': 'techstables_backend.serializers.CurrentUserSerializer'
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -104,6 +107,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware'
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'techstables_backend.urls'
 
