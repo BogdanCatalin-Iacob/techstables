d22931d00cb10dfb7991ee3b00f2df37c5c9738b
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-06-12 20:17:01 +0100

    Update allowed hosts

--- techstables_backend/settings.py
+++ techstables_backend/settings.py
@@ -38,7 +38,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = [
+    'localhost',
+    'https://techstables-2157aa5076c9.herokuapp.com']
 
 
 # Application definition
