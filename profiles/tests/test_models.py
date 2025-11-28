47b35cc334614ae6459736ea4286ad7c43fb2b5b
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-11-28 17:10:42 +0000

    List tests needed for profile model

---
+++ profiles/tests/test_models.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from ..models import Profile
+import time
+
+def test_auto_created_profile_on_user_creation(self):
+    pass
+
+def test_no_duplicate_profiles_on_user_update(self):
+    pass
+
+def test_profile_deletion_on_user_deletion(self):
+    pass
+
+def test_str_representation_of_profile(self):
+    pass
+
+def test_default_image_upload(self):
+    pass
+
+def test_ordering_by_profile_desc(self):
+    pass
+
+def test_updated_change_on_save(self):
+    pass
+
+def test_delete_user_deletes_profile(self):
+    pass
+
+def test_cannot_create_profile_for_the_same_user_twice(self):
+    pass
